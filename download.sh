4e51ad13b9ac12b10bfb43d7fa7961a2059d5bbe
krasow <krasow@u.northwestern.edu>
2024-09-13 14:10:35 -0500

    scripts

---
+++ download.sh
@@ -0,0 +1,72 @@
+# upmem support of legion
+git clone https://github.com/PrescienceLab/upmem-legion
+# test cases
+git clone https://github.com/krasow/upmem-test
+# upmem runtime
+wget http://sdk-releases.upmem.com/2024.1.0/ubuntu_22.04/upmem-2024.1.0-Linux-x86_64.tar.gz
+tar -xf upmem-2024.1.0-Linux-x86_64.tar.gz
+rm upmem-2024.1.0-Linux-x86_64.tar.gz
+# patch upmem runtime
+cat > ./upmem-2024.1.0-Linux-x86_64/share/upmem/include/stdlib/stddef.h <<EOF
+/* Copyright 2020 UPMEM. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Modified by David Krasowska krasow@u.northwestern.edu to change types to support C++ 
+ * compilation for the Legion runtime system.
+ */
+
+#ifndef _DPUSYSCORE_STDDEF_H_
+#define _DPUSYSCORE_STDDEF_H_
+
+/**
+ * @file stddef.h
+ * @brief Defines some standard types.
+ */
+
+/**
+ * @brief The type for the result of subtracting two pointers.
+ */
+typedef int ptrdiff_t;
+
+/**
+ * @brief The type for the result of the <code>sizeof</code> operator.
+ *
+ * @internal No memory or object in the DPU can be more than megabytes of data.
+ *           As a consequence, we can restrict the sizes to 32 bits, which is
+ *           already a lot.
+ */
+typedef unsigned int size_t; // change for legion
+/**
+ * @brief Value whose alignment requirement is at least as strict (as large) as that of every scalar type.
+ */
+typedef unsigned long int max_align_t;
+
+#ifndef __cplusplus // compiling with c++ for legion
+/**
+ * @brief The type for wide-character codes.
+ */
+typedef unsigned int wchar_t;
+/**
+ * @def NULL
+ * @brief The null pointer constant.
+ */
+#define NULL ((void *)0) 
+#else
+#define NULL ((char *)0)
+#endif
+
+/**
+ * @def offsetof
+ * @hideinitializer
+ * @brief Offset in bytes to the structure member, from the beginning of its structure.
+ *
+ * @param st the structure
+ * @param m the member name
+ *
+ * @internal Raw version of offsetof, should be enough in our context, with all the underlying risks.
+ */
+#define offsetof(st, m) ((size_t)(&((st *)0)->m))
+
+#endif /* _DPUSYSCORE_STDDEF_H_ */
+EOF
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# for ubuntu 22.04
+DIR_PATH=$( dirname -- "$( readlink -f -- "$0"; )"; )
+echo "$DIR_PATH"
+export LG_RT_DIR="$DIR_PATH/upmem-legion/runtime"
+source $DIR_PATH/upmem-2024.1.0-Linux-x86_64/upmem_env.sh simulator
+#source $DIR_PATH/upmem-2024.1.0-Linux-x86_64/upmem_env.sh
+cd $DIR_PATH/upmem-test
+make -j
+
+for (( i=1 ; i<= 32 ; i*=2 )); 
+do
+    ./upmem_test -ll:num_dpus $i -b $i
+done
+
+
